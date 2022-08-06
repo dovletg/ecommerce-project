2bbb9a2d72a545cff1ab8f3af91da4ceb973a5a0
Dovlet <dovletx@gmail.com>
2022-08-06 17:17:02 -0400

    spring boot app created, entity created

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/learn2code/ecommerce/entity/Product.java
@@ -0,0 +1,50 @@
+package com.learn2code.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data //automatically generates getter/setter methods
+public class Product {
+   // 2. add mapping
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    // 1.from product table from mysql
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp    // Hibernate will auto manage the timestap
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+}
