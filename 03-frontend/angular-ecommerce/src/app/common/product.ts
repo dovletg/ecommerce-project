bd6b49665040a9b6bd05c29099f0be8efaa8553f
Dovlet <dovletx@gmail.com>
2022-08-14 19:42:49 -0400

    Developed TS class for Product. created angular service. Updated app.modue.ts imported httpClientModule.

---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,14 @@
+export class Product {
+    // list up all the properties from json files springBoot
+    constructor(public sku: string,
+                public name: string,
+                public description: string,
+                public unitPrice: number,
+                public imageUrl: string,
+                public active: boolean,
+                public unitsInStock: number,
+                public dateCreated: Date,
+                public lastUpdated: Date
+        ) {
+        }
+}
