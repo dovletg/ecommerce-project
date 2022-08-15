905fffa67c11797f72a2120f9641ec74f69ee939
Dovlet <dovletx@gmail.com>
2022-08-15 12:07:51 -0400

    modified product.service.ts to map the JSON data from Spring Data REST to Product array

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  // inject HttpClient
+  constructor(private httpClient: HttpClient) { }
+
+  //new method map the JSON data from Spring data REST to Product array
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+// add supporting interface it unwraps the JSON from Spring Data REST _embedded entry
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
