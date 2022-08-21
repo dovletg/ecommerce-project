588e974534c3be0ee3c4df597e1392c86719ccfb
Dovlet <dovletx@gmail.com>
2022-08-21 12:25:54 -0400

    Added CrossOrigin support to SpringBoot app

--- 02-backend/spring-boot-ecommerce/src/main/java/com/learn2code/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/learn2code/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.learn2code.ecommerce.dao;
 import com.learn2code.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 // "prodCateg"=Name of JSON entry. "prod-categ"=/product-category path
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
--- 02-backend/spring-boot-ecommerce/src/main/java/com/learn2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/learn2code/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.learn2code.ecommerce.dao;
 
 import com.learn2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") //accept calls from web browser scripts for this origin
 // Product from Entity, Long from Primary Key
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
