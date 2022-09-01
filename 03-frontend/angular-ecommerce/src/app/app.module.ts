758d0c8244eeb4d43da9bf2df5ffce2c14105369
Dovlet <dovletx@gmail.com>
2022-09-01 09:49:15 -0400

    defined routes and configured Router based on my routes

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [ //path to match. When path matches, create new instance of component
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}, // This is the generic wildcard. It will match on anything that didnt match above routes.
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +21,7 @@ import { ProductService } from './services/product.service';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes), // routes comes from line 10 const routes:
     BrowserModule,
     HttpClientModule
   ],
