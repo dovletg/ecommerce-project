db58e3fa6f5546b5d6c16da1dd9f198f2da42fc1
Dovlet <dovletx@gmail.com>
2023-01-02 14:03:43 -0500

    Updated ProductListComponent for setting page size

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit {
 
   // new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 50;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
   
 
@@ -95,5 +95,11 @@ export class ProductListComponent implements OnInit {
                                                     this.theTotalElements = data.page.totalElements;
                                                   }
                                                 );
+  }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }
